f6b6f98abb493e46eb23b79a3c579d70b1e82624
panilitbsp <myrasivaa@gmail.com>
2026-04-01 21:18:35 +0700

    Fix 4.0 Play Store rejection: Tambah dialog Prominent Disclosure dan hapus QUERY_ALL_PACKAGES

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.siva.tamengtautan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
